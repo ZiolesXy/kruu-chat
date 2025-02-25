93e81bf219e90facb50bdffb36a6524d867f0461
ZiolesXy <eaglegaming3605@gmail.com>
2025-02-25 11:50:00 +0700

    API Fix 3

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -14,7 +14,7 @@ const systemInstruction = "anda adalah KruAI, AI Desainer video";
 
 class GeminiService {
   constructor() {
-    this.model = genAI.GenerativeModel({ 
+    this.model = genAI.getGenerativeModel({ 
       model: "gemini-2.0-flash-thinking-exp-01-21h",
       generationConfig: generationConfig,
     });
